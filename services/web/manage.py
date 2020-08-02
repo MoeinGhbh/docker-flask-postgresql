4266983a6908e11432a22cc24a1e117f84a9dfb9
= <=>
2020-08-02 14:33:46 +0200

    add flask and cli

---
+++ services/web/manage.py
@@ -0,0 +1,15 @@
+'''
+to configure the Flask CLI tool to run and manage the app from the command line
+'''
+from flask.cli import FlaskGroup
+
+from project import app
+
+
+# we created a new FlaskGroup instance to extend 
+# the normal CLI with commands related to the Flask app.
+cli = FlaskGroup(app)
+
+
+if __name__=='__main__':
+    cli()
